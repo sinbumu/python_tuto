085d8e0410b5ae8a2d2d131bdccd8fd6d9517970
sinbumu <sinbumu2@blocko.io>
2024-09-09 22:41:28 +0900

    2주차 강의

---
+++ class/l1.py
@@ -0,0 +1,44 @@
+print("test")
+print(3+5)
+print(3*2563)
+5-10
+
+#기본적인 문법 설명들인데...
+#다들 비슷비슷 하고 하나 특이점은 파이썬은 제곱 표현을
+print(2**10)
+#이런식으로 ** 함
+'''
+ㅇ
+'''
+
+name = "아토믹"
+rank = "국가권력급"
+
+print("아이 엠 ", name)
+print(rank, "뭉탱이")
+
+#input()
+
+catName = input("고양이 이름을 입력")
+print("catName = ",catName)
+
+num = input("숫자를 입력")
+print(100-int(num))
+
+#논리 연산자
+'''
+and
+or
+not
+'''
+
+#관계 연산자
+'''
+== equal
+!= not equal
+> greater than
+< less than
+>= greater than or equal to
+<= less than or equal to
+'''
+
---
+++ class/l1_1.py
@@ -0,0 +1,17 @@
+print(10+3)
+print(10-3)
+print(10*3)
+print(10/3)
+print(10//3)
+print(10%3)
+print(10**3)
+
+###
+
+a = 10
+b = 5
+
+###
+
+import keyword
+print(keyword.kwlist)
